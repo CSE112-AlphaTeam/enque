9046cf01fb3533d1084806604245db165f90d1db
Nicholas McDonald <nmmcdona@ucsd.edu>
2017-05-16 16:05:59 -0700

    Update. Should fail for video purposes.

--- test/test.js
+++ test/test.js
@@ -4,6 +4,6 @@ var foo_lib = require('../lib/auth.js');
 
 describe("Testing Unit Testing. ", function () {
   it("Return True", function () {
-    expect(true).toBe(true);
+    expect(true).toBe(false);
   });
 });
